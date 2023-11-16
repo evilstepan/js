fd8f919a5ba64d1de2590844407ad55342a48406
evilstepan <150423868+evilstepan@users.noreply.github.com>
2023-11-16 12:28:59 +0300

    Add files via upload

---
+++ einstein.js
@@ -0,0 +1,51 @@
+/*
+const myWeight = ('63');
+
+const speedLight = ('3e8');
+
+const emc = (myWeight*speedLight**2);
+
+console.log('энергия солнца',emc);
+
+let a = 1+2+3;
+alert(a);
+
+let c = 10;
+
+let d = 5;
+
+result = c+d;
+
+alert(result);
+
+
+let a = 10;
+let b = 2;
+
+alert(a+b);
+alert(a-b);
+alert(a/b);
+alert(a*b);
+
+
+
+let a = 1;
+
+let b = 2;
+
+let c = 3;
+
+alert(a+b+c);
+*/
+
+let a = 10;
+
+let b = 5;
+
+let c = (a-b);
+
+let d = 7;
+
+let result = (c+d);
+
+console.log(result);
---
+++ index.js
@@ -0,0 +1,43 @@
+'use strict'
+/*let money = prompt('ваш месячный доход ');
+
+let gus = confirm('вы гражданин рф?');
+
+alert  (`ваш доходэ ${money}`);
+
+let money = +prompt('ваш месячный доход');
+
+let profit = +prompt('мой дополнительный зароботок');
+
+let expenses = +prompt('расходы ежемсячные');
+
+let amount = +prompt('сумма обязательных расходов');
+
+let depozit = confirm('у вас есть вклад в банке?');
+
+const target = +prompt('цель в бабках');
+
+let budgetMonth = (money + profit - expenses - amount);
+
+console.log('траты в день',Math.floor(budgetMonth/30));
+
+console.log('ваш бюджет:',budgetMonth);
+
+console.log('за сколько накопим',Math.ceil(target/budgetMonth),'месяцев');
+
+const product = prompt('Назовите наименование товара');
+
+const amount = +prompt('Напишите кол-во товара');
+
+const categoty = prompt('Напишите категорию товара');
+
+const price = +prompt('Напишите цену товара');
+
+console.log('на складе',amount,'единицы',product,'На сумму', (price*amount),'деревянных рублей' );
+
+console.log(typeof amount);
+
+console.log(typeof categoty);
+
+console.log(typeof price);
+*/
